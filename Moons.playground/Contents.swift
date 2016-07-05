30aef98ba06c8dab1e24f8ce8b9e1129cc3ab24a
Uskinn <s.nevzor@yahoo.com>
2016-07-05 15:31:14 -0400

    Done

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -1,43 +1,38 @@
-func moonsOfJupiter() {
-    print("There are 67 moons orbiting Jupiter.")
-}
-
-moonsOfJupiter()
+//func moonsOfJupiter() {
+//    print("There are 67 moons orbiting Jupiter.")
+//}
+//
+//moonsOfJupiter()
 
 
 // planetDescription, take 1
 // The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
-/*
-func planetDescription(planet: String) {
-    print("There are 67 moons orbiting \(planet).")
+
+//func planetDescription(planet: String) {
+//   // print("There are 67 moons orbiting \(planet).")
+//}
+//
+//planetDescription("Jupiter")
+//
+//var planet = "Mars"
+//
+////planetDescription(planet)
+//
+//func greet(name: String, greeting: String) {
+//    
+//    print("\(name), \(greeting)")
+//    
+//}
+
+//greet("Sam", greeting: "Hey")
+
+func planetDecription(name: String, numberOfMoons: Int) {
+    
+    print("\(name) has \(numberOfMoons) moons")
 }
 
-planetDescription("Jupiter")
+planetDecription("Mars", numberOfMoons: 2)
 
-var planet = "Mars"
-
-planetDescription(planet)
-*/
-
-
-func greet(name: String, greeting: String) {
-    print("\(greeting), \(name)!")
-}
-
-
-func planetDescription(planet: String, numberOfMoons: Int) {
-    print("There are \(numberOfMoons) moons orbiting \(planet)")
-}
-
-// This line of code gives an error.
-// Uncomment it to see what the error is!
-//planetDescription("Jupiter", 67)
-
-planetDescription("Jupiter", numberOfMoons: 67)
-
-var planet = "Jupiter"
+var jupiter = "Jupiter"
 var moons = 67
-planetDescription(planet, numberOfMoons: moons)
-planet = "Mars"
-moons = 2
-planetDescription(planet, numberOfMoons: moons)
+planetDecription(jupiter, numberOfMoons: moons)
